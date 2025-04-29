3f663170ea8f1909bc35c332bbfaa299e1508043
Sanapathi Kishore Naidu <141444869+Mahii918@users.noreply.github.com>
2025-04-29 10:41:30 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treats all JS code as newer version
+
+// alert(3+3)   //we are using nodejs but not browser, but this only works in browser
+
+console.log(3
+    +
+    3)   //code readability should be high priority i.e. it should be console.log(3+3)
+    
+console.log("APS Yol")
+
+//DATA TYPES
+
+let name="Kishore"
+let age=23
+let cond=true
+
+// number =>  2 to power 53
+// bigint
+// string => ""
+// boolean  =>  true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+//object
+console.log(typeof "ABC")       //string
+console.log(typeof age)         //number
+console.log(typeof null)        //object
+console.log(typeof undefined)   //undefined
